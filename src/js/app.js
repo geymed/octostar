58921eb9a10c2da0d27d7c499e07c544e6a15c91
Trent Oswald <trentoswald@therebelrobot.com>
2014-06-30 13:37:27 -0600

    layout fix

--- src/js/app.js
+++ src/js/app.js
@@ -2,22 +2,28 @@
 
 // Start main logic
 var app,
-    services    = angular.module('constella.services',    []),
-    directives  = angular.module('constella.directives',  []),
-    filters     = angular.module('constella.filters',     []),
-    controllers = angular.module('constella.controllers', []);
-    
+  services = angular.module('constella.services', []),
+  directives = angular.module('constella.directives', []),
+  filters = angular.module('constella.filters', []),
+  controllers = angular.module('constella.controllers', []);
+
 app = angular.module('constella', ['constella.services',
-                                   'constella.directives',
-                                   'constella.filters',
-                                   'constella.controllers',
-                                   'ui.router',
-                                   'ngAnimate']);
+  'constella.directives',
+  'constella.filters',
+  'constella.controllers',
+  'ui.router',
+  'ngAnimate'
+]);
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
   $stateProvider
-    .state('main',  { url: '/',   page: 'main',   controller: 'mainCtrl',   templateUrl: 'templates/main.html' });
+    .state('main', {
+      url: '/',
+      page: 'main',
+      controller: 'mainCtrl',
+      templateUrl: 'templates/main.html'
+    });
 });
 app.constant('loggedIn', 'true');
 app.value('mobile', false);
